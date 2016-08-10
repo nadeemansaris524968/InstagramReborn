f0f86b9f23c4d10398925e50add83c6708be656f
Nadeem Ansari <nadeemansari@nadeems-macbook-air.local>
2016-08-10 18:51:48 -0500

    Users can now post their images and their followers will be able to see it.

---
+++ Cell.swift
@@ -0,0 +1,20 @@
+//
+//  Cell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Nadeem Ansari on 8/10/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    @IBOutlet weak var postedImage: UIImageView!
+    
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var message: UILabel!
+    
+
+}
